6ebf7b08d79f285f88cbd02731002d67b7f56551
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-21 15:41:45 +1000

    router,routes,route

--- src/routers/Layout.jsx
+++ src/routers/Layout.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from '../views/Home'
+import About from '../views/About'
 
 export default class Layout extends Component {
   render() {
     return (
-      <div>
-        <h3>Layout</h3>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/about" element={<About/>} />
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+
+      </Router>
+     
     )
   }
 }
