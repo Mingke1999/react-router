57f2a2fb0418c94bbcb50abe829e0e34f8556220
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-24 15:30:25 +1000

    Link jump

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ HashRouter
 /#/path use anchor <a href="#/about"></a>
 BrowseRouter
 /path use History.pushState() from H5
+
+Link will be rendered as <a>
+<Link to="path"> equals to <a href="#/path"> not recommend
--- src/routers/Links.jsx
+++ src/routers/Links.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom"
+import "./Links.css"
 
 export default class Links extends Component {
   render() {
     return (
-        <ol>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-        </ol>
+        <div className='nav'>
+            <ol>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ol>
+        </div>
     )
   }
 }
